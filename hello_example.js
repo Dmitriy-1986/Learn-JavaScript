8e55e8737d79483a9f222ed1cb4a70e9054aac7a
Dmitriy-1986 <52016913+Dmitriy-1986@users.noreply.github.com>
2021-03-31 21:30:55 +0300

    Update hello_example.js

--- hello_example.js
+++ hello_example.js
@@ -9,5 +9,8 @@ class HelloMessage extends React.Component {
   }
 }
 
-ReactDOM.render(React.createElement(HelloMessage, { name: "Taylor" }),
-document.getElementById('#hello_example'));
+//ReactDOM.render(React.createElement(HelloMessage, { name: "Taylor" }),
+//document.getElementById('#hello_example'));
+
+const domHello = document.querySelector('#hello_example');
+ReactDOM.render(HelloMessage, { name: "Taylor" });
