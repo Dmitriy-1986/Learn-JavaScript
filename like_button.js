f26124118ca8a51d49646845e0fbe8af0b543eac
Dmitriy-1986 <52016913+Dmitriy-1986@users.noreply.github.com>
2021-03-31 20:31:25 +0300

    Update like_button.js

--- like_button.js
+++ like_button.js
@@ -1,7 +1,22 @@
-const name = 'Иван-Царевич';
-const element = <h1>Здравствуй, {name}!</h1>;
+'use strict';
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
