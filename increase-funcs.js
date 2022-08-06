4dfccdfda4c862c8a6aa835eda7e377ef828c5c4
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-08-06 22:05:23 +0300

    Update increase-funcs.js

--- increase-funcs.js
+++ increase-funcs.js
@@ -10,3 +10,14 @@ function increasePersonAge(person) {
 
 increasePersonAge(personOne);
 console.log(personOne.age);
+/* Не рекомендовано внутри функции мутировать внешний объект */
+
+function increasePersonAgeTwo(person) {
+    const updatedPerson = Object.assign({}, person);
+    updatedPerson.age += 1;
+    return updatedPerson;
+}
+
+const updatedPersonOne = increasePersonAgeTwo(personOne);
+console.log(updatedPersonOne.age);
+console.log(personOne.age);
