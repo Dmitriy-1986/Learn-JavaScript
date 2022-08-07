a3a4ef4c83c01933c0915bf50955ef80b0940fc2
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-08-07 22:06:58 +0300

    Update example-promise-json.js

--- example-promise-json.js
+++ example-promise-json.js
@@ -2,3 +2,8 @@ fetch('https://dmitriy-1986.github.io/Learn-JavaScript/example.json')
     .then((response) => response.json())
     .then(json => console.log(json)) 
     .catch(error => console.error(error))
+//
+fetch('https://dmitriy-1986.github.io/Learn-JavaScript/example.json')
+    .then((response) => response.json())
+    .then(json => document.write(json.title)) //возвращает объект
+    .catch(error => console.error(error))
